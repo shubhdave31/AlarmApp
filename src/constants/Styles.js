9a3ae97a421d7ff4041debd2e880a995aff4214d
shubhdave31 <shubhdave31@users.noreply.github.com>
2025-12-27 18:20:35 +0530

    Initial setup with Expo Router and professional structure

---
+++ src/constants/Styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import Colors from './Colors';
+
+export const GlobalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+});
+
+export const Theme = Colors;
